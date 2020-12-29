ccf1a23d4f783da41871f8a46e467a519aaecf4d
BALAJI <balajisb147@gmail.com>
2020-12-29 16:12:58 +0530

    Adding CORS requirement

--- TaskManager/settings.py
+++ TaskManager/settings.py
@@ -41,9 +41,11 @@ INSTALLED_APPS = [
     'webapp',
     'UICtrl',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
